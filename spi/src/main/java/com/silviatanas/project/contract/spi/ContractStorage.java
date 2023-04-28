468d35d8f201388692f8a71ca6b253239a71a7d8
silviatanas <silviatanas@abv.bg>
2023-04-28 11:42:52 +0300

    SPI ContractStorage update

--- spi/src/main/java/com/silviatanas/project/contract/spi/ContractStorage.java
+++ spi/src/main/java/com/silviatanas/project/contract/spi/ContractStorage.java
@@ -1,7 +1,9 @@
 package com.silviatanas.project.contract.spi;
 
+import java.util.List;
+
 public interface ContractStorage {
     public void storeContract(Contract contract);
-    public void listContract(Contract contract);
-    public void getByID(); // ?
+    public void listContract(List<Contract> contractList);
+    public void getByID(Contract contract);
 }
