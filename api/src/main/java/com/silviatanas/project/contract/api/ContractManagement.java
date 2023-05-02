e06bbfafde8dfb94edf1a2a95d46d19443ff523d
silviatanas <silviatanas@abv.bg>
2023-05-02 11:36:16 +0300

    added ContractManagement methods and their implementation

--- api/src/main/java/com/silviatanas/project/contract/api/ContractManagement.java
+++ api/src/main/java/com/silviatanas/project/contract/api/ContractManagement.java
@@ -1,4 +1,11 @@
 package com.silviatanas.project.contract.api;
 
+import java.util.List;
+
 public interface ContractManagement {
+    public void addContract(int contractID);
+    public void removeContract(int contractID);
+    public List<Contract> listContracts();
+    public Contract getContractByID(int contractID);
+    public void updateContract(Contract contract);
 }
---
+++ impl/src/main/java/com.silviatanas.project.contract.impl/ImplContractManagement.java
@@ -0,0 +1,33 @@
+package com.silviatanas.project.contract.impl;
+
+import com.silviatanas.project.contract.api.Contract;
+import com.silviatanas.project.contract.api.ContractManagement;
+
+import java.util.List;
+
+public class ImplContractManagement implements ContractManagement {
+    @Override
+    public void addContract(int contractID) {
+
+    }
+
+    @Override
+    public void removeContract(int contractID) {
+
+    }
+
+    @Override
+    public List<Contract> listContracts() {
+        return null;
+    }
+
+    @Override
+    public Contract getContractByID(int contractID) {
+        return null;
+    }
+
+    @Override
+    public void updateContract(Contract contract) {
+
+    }
+}
