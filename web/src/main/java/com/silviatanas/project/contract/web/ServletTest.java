0172cf6434ccfcff9036d1b05c68a1d3a7fd61be
silviatanas <silviatanas@abv.bg>
2023-05-31 15:17:55 +0300

    second draft Servlet with url query

--- web/src/main/java/com/silviatanas/project/contract/web/ServletTest.java
+++ web/src/main/java/com/silviatanas/project/contract/web/ServletTest.java
@@ -1,6 +1,25 @@
 package com.silviatanas.project.contract.web;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/home")
 public class ServletTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        String name = request.getParameter("name");
+
+        PrintWriter message = response.getWriter();
+        if (name == null) {
+            message.println("Welcome visitor");
+        } else {
+            message.println("Welcome " + name);
+        }
+    }
 }
