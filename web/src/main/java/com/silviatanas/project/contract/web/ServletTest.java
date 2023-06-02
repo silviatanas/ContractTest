cc55ae549c7fb650faa1923656f66a885f4b904f
silviatanas <silviatanas@abv.bg>
2023-06-02 15:27:33 +0300

    added hit counter to Servlet

--- web/src/main/java/com/silviatanas/project/contract/web/ServletTest.java
+++ web/src/main/java/com/silviatanas/project/contract/web/ServletTest.java
@@ -4,22 +4,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSessionEvent;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/home")
 public class ServletTest extends HttpServlet {
+    private int hitCounter;
+
+    public void init() {
+        hitCounter = 0;
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        hitCounter++;
         String name = request.getParameter("name");
-
         PrintWriter message = response.getWriter();
+
         if (name == null) {
             message.println("Welcome visitor");
         } else {
             message.println("Welcome " + name);
         }
+        message.println("Hit counter: ");
+        message.println(hitCounter);
     }
 }
